fb0b1558a0e9f86b02735a4225dd8710172137f5
Panyushenko <>
2018-03-31 00:42:03 +0300

    add Table View Controller, create file(TableViewController, tableViewCell)

---
+++ TableContollerDemo/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TableContollerDemo
+//
+//  Created by Panyushenko on 31.03.2018.
+//  Copyright © 2018 Artyom Panyushenko. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
